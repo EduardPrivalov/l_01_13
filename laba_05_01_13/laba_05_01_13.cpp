9b5b27d911ac03b1a3b7301e87a68f5ef2c4da6b
EduardPrivalov <ed_230101@mail.ru>
2021-10-30 18:19:23 +0300

    create solution

---
+++ laba_05_01_13/laba_05_01_13.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <String>
+#include "Keeper.h"
+#include "Figure.h"
+#include "FlatFigure.h"
+#include "VolumetricFigure.h"
+#include "List.h"
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
+
+/*
+
+
+Разработать класс Keeper, который реализует контейнер для хранения и 			[Keeper.h]
+обработки объектов. Класс Keeper должен обеспечивать следующие функции: 
+	▪ добавление и удаление производных объектов абстрактного класса Base		[deleteFigure(size_t)] [addFigure()]
+	▪ полное сохранение себя в файле;											[saveToFile(string)]
+	▪ полное восстановление себя из файла.										[loadFromFile(string)]
+
+Класс Фигур содержит данные о плоских и объемных фигур. 						[Figure.h]
+Для плоской:  тип, размеры, площадь, рисунок на экране.							[FlatFigure.h]
+Для объемной: тип, размеры, объем,   рисунок на экране в виде плоской фигуры.	[VolumetricFigure.h]
+
+*/
